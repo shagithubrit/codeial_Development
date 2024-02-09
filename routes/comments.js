0b1f970ddc6bb63e55a69d4b5875ed661505dba2
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-02-09 15:17:03 +0530

    comments section updated

--- routes/comments.js
+++ routes/comments.js
@@ -4,7 +4,11 @@ const passport = require('passport');
 
 const commentsController = require('../controllers/comments_controller');
 
+// we using post because we changing and updating sometging in database via frontend 
 router.post('/create', passport.checkAuthentication, commentsController.create);
+//  we using get request because we only need to delete it directly no change in database is to be done ...
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
+
 
 
 module.exports = router;
