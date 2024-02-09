3da36caafe46cc76fd4de4224a30c3d8e25e5124
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-02-09 20:14:10 +0530

    added user link in home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 const Post = require("../models/post");
-
+const User =require("../models/user");
 module.exports.home = function (req, res) {
   console.log(req.cookies);
   res.cookie("user_id", 25);
@@ -29,9 +29,13 @@ module.exports.home = function (req, res) {
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
+       
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users: users
+            });
         });
     })
 
