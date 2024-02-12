67b2f3d981302a042095834153c42b051b4b050e
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-02-12 17:48:44 +0530

    ajax call to send the data

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,15 +3,26 @@ const Comment = require('../models/comment');
 
 module.exports.create=async function(req,res){
     try{
-        Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
-            return res.redirect('back');
-    }
-    catch(err){
-        console.log('Error' , err);
-        return;
+        
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
+        return res.redirect('back');
+
+    }catch(err){
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }  
 module.exports.destroy=  async function(req, res){
