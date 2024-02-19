6f74dfc3673350d2440b00901186a13971b3c5f5
Shashwat Kapoor <shashwatquora2000@gmail.com>
2024-02-19 22:11:52 +0530

    some extra changes done

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 
 const usersController = require('../controllers/users_controller');
+
 router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
 router.post('/update/:id', passport.checkAuthentication, usersController.update);
 
@@ -20,5 +21,11 @@ router.post('/create-session', passport.authenticate(
 
 
 router.get('/sign-out', usersController.destroySession);
+
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+
+
 
 module.exports = router;
